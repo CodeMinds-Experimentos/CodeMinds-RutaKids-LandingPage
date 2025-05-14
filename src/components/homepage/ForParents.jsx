ccc9509f9873875e216ea3631f54714906795a8f
Belen Ramos <u202216246@upc.edu.pe>
2025-05-14 18:39:11 -0500

    .

--- src/components/homepage/ForParents.jsx
+++ src/components/homepage/ForParents.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import { FaBell, FaMapMarkedAlt, FaCheckCircle, FaClock } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
+import ios from '/src/assets/images/ios.png';
+import android from '/src/assets/images/android.png';
 
 const icons = [
     <FaBell className="text-blue-600 text-xl" />,
@@ -55,7 +57,7 @@ export default function ParentsSection({ forwardedRef }) {
                         transition={{ duration: 1, ease: 'easeOut' }}
                     >
                         <img
-                            src="src/assets/images/ios.png"
+                            src={ios}
                             alt="Mockup atras"
                             className="w-40 sm:w-48 md:w-56 lg:w-64 relative z-0 sm:translate-x-3 translate-x-12"
                         />
@@ -68,7 +70,7 @@ export default function ParentsSection({ forwardedRef }) {
                         transition={{ duration: 1, ease: 'easeOut', delay: 0.3 }}
                     >
                         <img
-                            src="src/assets/images/android.png"
+                            src={android}
                             alt="Mockup adelante"
                             className="w-40 sm:w-48 md:w-56 lg:w-64 mx-auto sm:-translate-x-32 sm:translate-y-16 -translate-x-10 translate-y-10"
                         />
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/CodeMinds-RutaKids-LandingPage",
+  base: '/CodeMinds-RutaKids-LandingPage/',
   plugins: [react()],
 })
